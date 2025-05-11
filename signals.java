58123673c0cd30e87ea74d13b5d96e5778c6afcd
Elijah Wilson <123503278+esw9386@users.noreply.github.com>
2025-05-10 23:12:28 -0400

    Delete signals.java

--- signals.java
+++
@@ -1,8 +0,0 @@
-package JParty;
-public class signals {
-    static final String WAITING = "0"; // waiting for teams to join
-    static final String CLOSED = "1"; // close buzzer
-    static final String OPEN = "2"; // open buzzer
-    static final String OVER = "3"; // game over
-    // static final String NEXT = "4";
-}
